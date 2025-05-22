28e0bebe00218c1968a0ee9ae205192532a189be
Deepanshu <137703927+Deepanshuyadav05@users.noreply.github.com>
2025-05-22 09:40:54 +0530

    check even odd

---
+++ 06_Functions/Problem_1.js
@@ -0,0 +1,11 @@
+//create a function to check if the number is even or odd
+
+function isEvenOrOdd(num){
+    if(num%2===0){
+        console.log("number is even")
+    }
+    else{
+        console.log("number is odd")
+    }
+}
+isEvenOrOdd(23);
